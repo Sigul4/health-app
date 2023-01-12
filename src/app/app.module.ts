54e14485e12c56d43a933c69bd12b90fce143ae7
Sigul4 <romannamor637@gmail.com>
2023-01-12 10:27:40 +0200

    added init icons service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitIconsService } from './services/init-icons.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    InitIconsService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (InitIconsService: InitIconsService) => InitIconsService.initIcons(),
+      deps: [InitIconsService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
