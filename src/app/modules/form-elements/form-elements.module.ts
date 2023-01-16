7c630c9b6dcbcd6a0d97d8a7812a890c69e72b8a
Sigul4 <romannamor637@gmail.com>
2023-01-16 13:35:58 +0200

    added datapicker form-element

--- src/app/modules/form-elements/form-elements.module.ts
+++ src/app/modules/form-elements/form-elements.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 import { IConfig } from 'ngx-mask/lib/config';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DatePickerComponent } from './components/datepicker/datepicker.component';
 import { InputComponent } from './components/input/input.component';
 import { SelectComponent } from './components/select/select.component';
 
@@ -13,12 +14,12 @@ const maskConfigFunction: () => Partial<IConfig> = () => {
 };
 
 @NgModule({
-  declarations: [InputComponent, SelectComponent],
+  declarations: [InputComponent,DatePickerComponent, SelectComponent],
   imports: [
     FormsModule,
     SharedModule,
     NgxMaskModule.forRoot(maskConfigFunction),
   ],
-  exports: [InputComponent, SelectComponent],
+  exports: [InputComponent,DatePickerComponent, SelectComponent],
 })
 export class FormElementsModule {}
